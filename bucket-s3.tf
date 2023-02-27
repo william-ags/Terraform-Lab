debb66a65af2765f55f7f8ed445a1ac43548eff7
william-ags <120600734+william-ags@users.noreply.github.com>
2023-02-27 11:47:02 -0300

    Code-modulo-terraform

--- bucket-s3.tf
+++ bucket-s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "teste" {
-  bucket = "infra-aws-will"
+  bucket = "infra-aws-terraform"
 
 }
--- main.tf
+++ main.tf
@@ -1,14 +1,37 @@
 terraform {
-    required_version = ">1.0.0"
+
+  required_version = ">=1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">=3.0.0"
     }
+
+  }
+
+
+  backend "s3" {
+    bucket = "infra-aws-will"
+    key    = "aws-ec2-modulo-local/terraform.tfstate"
+    region = "us-east-1"
+
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = var.location
+
+
+  default_tags {
+    tags = {
+      owner      = "William"
+      managed-by = "terraform"
+    }
   }
+}
+
+
+module "network" {
+  source = "./network"
+}
